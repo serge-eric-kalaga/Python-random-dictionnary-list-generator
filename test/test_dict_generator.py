595e2da07f942edc48bf142fa8d01b8a599254d1
serge-eric-kalaga <kalagaserge4@gmail.com>
2022-10-25 14:37:41 +0200

    Test ok

--- test/test_dict_generator.py
+++ test/test_dict_generator.py
@@ -1,15 +1,15 @@
 import unittest
 from RandomListOrDictGenerator.list_or_dict_generator import RandomListOrDictionnary
   
-class ListGeneratorTest(unittest.TestCase):
+class DictGeneratorTest(unittest.TestCase):
   
     def setUp(self):
         self.size = 5
         self.clss = RandomListOrDictionnary(size=self.size, key_length=3, values_length=5)
-        self.list = self.clss.createRandomList()
+        self.dic = self.clss.createRandomDictionnary()
     
     def test_test(self):        
-        self.assertTrue(len(self.list)==self.size)
+        self.assertTrue(len(self.dic)==self.size)
   
 if __name__ == '__main__':
     unittest.main()
